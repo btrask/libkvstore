331223a585162ffb37ca5cf9eae4cec8a644c7d5
Ben Trask <bentrask@comcast.net>
2016-08-03 10:03:30 -0400

    Add macro for computing worst case blob storage size.

--- src/db_schema.h
+++ src/db_schema.h
@@ -66,6 +66,7 @@ void db_bind_string(DB_val *const val, char const *const str, DB_txn *const txn)
 void db_bind_string_len(DB_val *const val, char const *const str, size_t const len, int const nulterm, DB_txn *const txn);
 
 // Blobs are always stored inline. The worst-case encoded length is len*2+2.
+#define DB_BLOB_MAX(n) ((n)*2+2)
 size_t db_read_blob(DB_val *const val, unsigned char *const out, size_t const max);
 void db_bind_blob(DB_val *const val, unsigned char const *const buf, size_t const len);
 
